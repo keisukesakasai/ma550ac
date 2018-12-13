7419e765aa0708d5795993b5f30dad5b47bbbfdf
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-12-13 17:58:52 +0900

    info: tested

--- scripts/tiltmeter.py
+++ scripts/tiltmeter.py
@@ -5,4 +5,26 @@ import numpy
 import datetime
 import pickle
 
-sync_off = [0x23, 0x05, 0x10, 0x00, 0xe8, 0x03, 0x00, 0x00]
+import rospy
+import std_msgs
+
+def str2list(param):
+    return param.strip('[').strip(']').split(',')
+
+class tilt_controller(object):
+
+    def __init__(self):
+        self.nid = rospy.get_param('~nid')
+        self.datafmt = rospy.get_param('datafmt')
+        self.synctime = rospy.get_param('~synctime')
+        self.sync_producer_nid = rospy.get_param('~sync_producer_nid')
+        print(self.nid)
+        print(self.datetime)
+        print(self.synctime)
+        print(self.sync_producer_nid)
+
+if __name__ == '__main__':
+    rospy.init_node('tiltmeter')
+    ctrl = tilt_controller()
+    ctrl.start_thread_ROS()
+    rospy.spin()
