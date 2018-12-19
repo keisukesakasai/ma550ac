d690618884c95f76d8c71e428daacf7ca28141f9
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-12-20 00:21:48 +0900

    bugfix

--- launch/start.sh
+++ launch/start.sh
@@ -1,3 +1,6 @@
+source /opt/ros/kinetic/setup.bash
+source ~/ros/devel/setup.bash
+export ROS_IP=172.20.0.20
 export ROS_MASTER_URI=http://172.20.0.20:11311
 export ROSLAUNCH_SSH_UNKNOWN=1
-roslaunch start.launch
+roslaunch _start.launch
---
+++ launch/start_2.sh
@@ -0,0 +1,4 @@
+export ROS_IP=172.20.0.33
+export ROS_MASTER_URI=http://172.20.0.20:11311
+roslaunch ma550ac_2.launch
+roslaunch ma550ac_relay_2.launch
