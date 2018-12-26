27dcc49349f95c80bee43959bd0319f46eeca7bf
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-12-26 18:19:35 +0900

    tpdo operation.

--- scripts/get_tpdo_config.py
+++ scripts/get_tpdo_config.py
@@ -1,8 +1,10 @@
 import os
+import sys
 import can
 import time
 import pprint
 
+cnt_node = sys.argv()[1]
 all_nid = 0x000
 base_nid = 0x600
 restart_data = [0x81] + [0x00] * 7
@@ -25,13 +27,11 @@ msg = can.Message(
     data=restart_data,
     extended_id=False
     )
-bus.send(msg)
-t0 = time.time()
-t1 = t0 + 3. # 1 sec. after
-while True:
-    if 
+bus.send(msg) # restart
+for i in range(cnt_node+1):
     d = bus.recv()
-    nid = int(hex(d.arbitration_id)[-1])
+    if d.arbitration_id == 0x000: continue
+    nid = int(hex(d.rbitration_id)[-1])
     nid_list.append(nid)
 
 print(sorted(nid_list))
