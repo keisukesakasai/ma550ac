0a8b09cd31956165e96a0000f3a25f4f39a872af
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-01-09 18:27:30 +0900

    develop logger

--- scripts/logger.py
+++ scripts/logger.py
@@ -13,28 +13,56 @@ from std_msgs.msg import Float64
 from std_msgs.msg import String
 
 
+# --
+name = 'tilt_logger'
+data_dir = '/home/amigos/data/experiments/'
+save_dir = os.path.join(data_dir, name)
+# --
+
 class logger(object):
 
     def __init__(self):
-        self.fn = ''
         self.xtilt = [0.] * 4
         self.ytilt = [0.] * 4
         self.ztilt = [0.] * 4
         self.temp = [0.] * 4
+        exp_time = datetime.datetime.fromtimestamp(float(time.time()))
+        ymd = exp_time.strftime('%Y%m%d_')
+        hms = exp_time.strftime('%H%M%S')
+        self.saveto = os.path.join(save_dir, ymd + hms)
+        os.makedirs(self.saveto)
+        self.filename_datetime = self.saveto + '/datetime.txt'
+        self.filename_xtilt = self.saveto + '/xtilt.txt'
+        self.filename_ytilt = self.saveto + '/ytilt.txt'
+        self.filename_ztilt = self.saveto + '/ztilt.txt'
+        self.filename_temp = self.saveto + '/temp.txt'
+        print('[INFO] File open (dir¡§{})'.format(saveto))
+
+        f_datetime = open(self.filename_datetime, 'a')
+        f_xtilt = open(self.filename_xtilt, 'a')
+        f_ytilt = open(self.filename_ytilt, 'a')
+        f_ztilt = open(self.filename_ztilt, 'a')
+        f_temp = open(self.filename_temp, 'a')
+
+        f_datetime.close()
+        f_xtilt.close()
+        f_ytilt.close()
+        f_ztilt.close()
+        f_temp.close()
 
     def callback_xtilt(self, req, idx):
 
-        self.xtit[idx] = req.data
+        self.xtilt[idx] = req.data
         return
 
     def callback_ytilt(self, req, idx):
 
-        self.ytit[idx] = req.data
+        self.ytilt[idx] = req.data
         return
 
     def callback_ztilt(self, req, idx):
 
-        self.ztit[idx] = req.data
+        self.ztilt[idx] = req.data
         return
 
     def callback_temp(self, req, idx):
@@ -43,10 +71,42 @@ class logger(object):
         return
 
     def log(self):
-        pass
+
+        datetime = str(time.time()) + '\n'
+        xtilt = ' '.join(map(str, self.xtilt)) + '\n'
+        ytilt = ' '.join(map(str, self.ytilt)) + '\n'
+        ztilt = ' '.join(map(str, self,ztilt)) + '\n'
+        temp = ' '.join(map(str, self.temp)) + '\n'
+
+        f_datetime = open(self.filename_datetime, 'a')
+        f_xtilt = open(self.filename_xtilt, 'a')
+        f_ytilt = open(self.filename_ytilt, 'a')
+        f_ztilt = open(self.filename_ztilt, 'a')
+        f_temp = open(self.filename_temp, 'a')
+
+        f_datetime.write(datetime)
+        f_xtilt.write(xtilt)
+        f_ytilt.write(yrilt)
+        f_ztilt.write(ztilt)
+        f_temp.write(temp)
+
+        f_datetime.close()
+        f_xtilt.close()
+        f_ytilt.close()
+        f_ztilt.close()
+        f_temp.close()
+
+        time.sleep(1e-2) # 10 msec.
 
 
 if __name__ == '__main__':
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+        pass
+
+    st = logger()
+    st.start_thread()
+    rospy.init_node(name)
     xtilt_sub_list = [rospy.Subscriber('/ma550ac_nid{0}_xtilt_arcsec'.format(nid),
                                        Float64,
                                        st.callback_xtilt,
@@ -67,4 +127,5 @@ if __name__ == '__main__':
                                        st.callback_temp,
                                        callback_args = idx)
                       for idx, nid in enumerate(range(4), start=1)]
+    rospy.spin()
 
